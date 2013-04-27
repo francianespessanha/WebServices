a27345b54d23adfd17acb962bbf9887ba0526f28
duduccarneiro <duduccarneiro@gmail.com>
2013-04-27 13:47:46 -0300

    Adicionada a função consultarAreceber

--- grupo10/servicoContasAReceber.py
+++ grupo10/servicoContasAReceber.py
@@ -6,8 +6,8 @@
 #deletarAreceber(codigoAreceber)
 
 #from SOAPpy import SOAPProxy
-arquivo = 'usuarios.txt'
-
+arquivo = 'basededados.txt'
+#conta = {'codigoAreceber':'0001','codigoVenda':'0001','dataVencimento':'20/05/2013','dataPagamento':'','status':'pendente'}
 def cadastrarContaAreceber(contasAReceber):
     contasAReceber['codigoAreceber']
     #servico = SOAPProxy("http://localhost:8080")
@@ -27,8 +27,23 @@ def cadastrarContaAreceber(contasAReceber):
         #venda nao existe
         return False
     
-def conssultarAreceber(codigoAreceber):
-    pass
+def consultarAreceber(codigoAreceber):
+    try:
+        conexao = open(arquivo, 'r')
+    except:
+        return False
+    dados = conexao.read()
+    linhas = dados.split('\n')
+    for linha in linhas:
+        if linha == '':
+            break
+        arrayLinha = linha.split('|')
+        if arrayLinha[0] == codigoAreceber:
+            return converterLinhaParaDicionario(arrayLinha)
+    return False
     
 def deletarAreceber(codigoAreceber):
     pass
+
+def converterLinhaParaDicionario(array):
+    return {'codigoAreceber':array[0],'codigoVenda':array[1],'dataVencimento':array[2],'dataPagamento':array[3],'status':array[4]}
